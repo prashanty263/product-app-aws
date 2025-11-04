dec88383ed64b1863a18ad6b0db50af7a8e78be5
prashanty263 <prashanty263@gmail.com>
2025-11-04 16:59:46 +0530

    errormodel

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace product_app_aws.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
